b14ac96496ffcfc5297338cc75d746565bd96003
ATyutyunkova <reptilemind@yandex.ru>
2023-10-19 18:20:09 +0500

    generate docx document

---
+++ DocxPdfGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using DocxPdfGenerator.Services;
+
+namespace DocxPdfGenerator;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the file path with name of file and extension '.docx': ");
+        var filePath = Console.ReadLine();
+        if (string.IsNullOrEmpty(filePath))
+        {
+            Console.WriteLine("Error: Empty file path!");
+            return;
+        }
+
+        Console.WriteLine("Enter the message: ");
+        var msg = Console.ReadLine();
+        if (string.IsNullOrEmpty(msg))
+        {
+            msg = "test string";
+        }
+
+        OpenXmlGenerator.GenerateDocument(filePath, msg);
+    }
+}
---
+++ DocxPdfGenerator/Services/OpenXmlGenerator.cs
@@ -0,0 +1,26 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocxPdfGenerator.Services;
+
+public static class OpenXmlGenerator
+{
+    public static void GenerateDocument(string filepath, string msg)
+    {
+        using (var doc =
+               WordprocessingDocument.Create(filepath, DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+        {
+            // Add a main document part
+            var mainPart = doc.AddMainDocumentPart();
+
+            // Create the document structure and add some text
+            mainPart.Document = new Document();
+            var body = mainPart.Document.AppendChild(new Body());
+            var para = body.AppendChild(new Paragraph());
+            var run = para.AppendChild(new Run());
+
+            // String msg contains the text
+            run.AppendChild(new Text(msg));
+        }
+    }
+}
