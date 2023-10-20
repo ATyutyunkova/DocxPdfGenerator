81e21e11204f0a827206c9bc961338c99f86523e
ATyutyunkova <reptilemind@yandex.ru>
2023-10-20 11:24:35 +0500

    wip generate docx from xml file

--- DocxPdfGenerator/Program.cs
+++ DocxPdfGenerator/Program.cs
@@ -6,6 +6,24 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Enter a number: 1 - GenerateNewDocx, 2 - GenerateDocxFromXml");
+        var doWhat = Console.ReadLine();
+
+        switch (doWhat)
+        {
+            case "1":
+                GenerateNewDocx();
+                break;
+            case "2":
+                GenerateDocxFromXml();
+                break;
+            default:
+                return;
+        }
+    }
+
+    private static void GenerateNewDocx()
+    {
         Console.WriteLine("Enter the file path with name of file and extension '.docx': ");
         var filePath = Console.ReadLine();
         if (string.IsNullOrEmpty(filePath))
@@ -21,6 +39,22 @@ public static class Program
             msg = "test string";
         }
 
-        OpenXmlGenerator.GenerateDocument(filePath, msg);
+        OpenXmlGenerator.GenerateNewDocx(filePath, msg);
+    }
+
+    private static void GenerateDocxFromXml()
+    {
+        Console.WriteLine("Enter the XML file path with name of file and extension '.xml': ");
+        var xmlFilePath = Console.ReadLine();
+        if (string.IsNullOrEmpty(xmlFilePath))
+        {
+            Console.WriteLine("Error: Empty file path!");
+            return;
+        }
+
+        Console.WriteLine("Enter the output file path with name of file and extension '.docx': ");
+        var outputDocument = Console.ReadLine();
+
+        OpenXmlGenerator.GenerateDocxFromXml(xmlFilePath, outputDocument);
     }
 }
--- DocxPdfGenerator/Services/OpenXmlGenerator.cs
+++ DocxPdfGenerator/Services/OpenXmlGenerator.cs
@@ -5,7 +5,7 @@ namespace DocxPdfGenerator.Services;
 
 public static class OpenXmlGenerator
 {
-    public static void GenerateDocument(string filepath, string msg)
+    public static void GenerateNewDocx(string filepath, string msg)
     {
         using (var doc =
                WordprocessingDocument.Create(filepath, DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
@@ -21,6 +21,30 @@ public static class OpenXmlGenerator
 
             // String msg contains the text
             run.AppendChild(new Text(msg));
+        }
+    }
+
+    public static void GenerateDocxFromXml(string xmlFilePath, string outputDocument)
+    {
+        var templateDocument =
+            @"C:\Users\tyutyunkova\source\altecReposRider\DocxPdfGenerator\DocxPdfGenerator\Resources\test\ExampleDoc.docx";
+
+        using (var wordDoc = WordprocessingDocument.Open(templateDocument, true))
+        {
+            //get the main part of the document which contains CustomXMLParts
+            var mainPart = wordDoc.MainDocumentPart;
+
+            //delete all CustomXMLParts in the document. If needed only specific CustomXMLParts can be deleted using the CustomXmlParts IEnumerable
+            mainPart.DeleteParts<CustomXmlPart>(mainPart.CustomXmlParts);
+
+            //add new CustomXMLPart with data from new XML file
+            var myXmlPart = mainPart.AddCustomXmlPart(CustomXmlPartType.CustomXml);
+            using (var stream = new FileStream(xmlFilePath, FileMode.Open))
+            {
+                myXmlPart.FeedData(stream);
+            }
+
+            wordDoc.Clone(outputDocument);
         }
     }
 }
